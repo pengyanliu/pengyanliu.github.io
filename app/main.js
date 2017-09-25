b0c5536befc3f2ccf0953702954f4937e75868d6
baojian.xin <xinbaojian@126.com>
2017-09-25 22:31:58 +0800

    init

---
+++ app/main.js
@@ -0,0 +1,9 @@
+require('./css/stars.scss');
+require('./css/bubble.css');
+require('./css/code.css');
+require('./css/full.css');
+require('./css/cursor.css');
+const love = require('./js/go.js');
+window.onload = () => {
+ love.go();
+}
